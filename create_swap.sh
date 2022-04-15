c7726e622247f79f7b055c9a7e14fd8c982ed54f
Captain Snowball <22148496+SecureSnowball@users.noreply.github.com>
2022-04-15 23:28:56 +0530

    Update create_swap.sh

--- create_swap.sh
+++ create_swap.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+# Run it on VPS that have 1GB or less RAM
+
 sudo fallocate -l 1G /swapfile
 sudo dd if=/dev/zero of=/swapfile bs=1024 count=1048576
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
-echo '/swapfile swap swap defaults 0 0' >> /etc/fstab # for automatically using it on reboot
+echo '/swapfile swap swap defaults 0 0' | sudo tee -a /etc/fstab # for automatically using it on reboot
 sudo swapon --show # for verification
 sudo free -h # for verification
